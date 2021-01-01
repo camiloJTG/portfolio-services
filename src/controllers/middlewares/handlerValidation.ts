fc5bf36d6cec0eb00045dce941443a4e738f7de4
camiloJTG <camilojtg.dev@gmail.com>
2021-01-01 13:34:23 -0300

    style: Refactor code with prettier

--- src/controllers/middlewares/handlerValidation.ts
+++ src/controllers/middlewares/handlerValidation.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import { SchemaMap, ValidationError, object } from 'joi';
+import { SchemaMap, object } from 'joi';
 //import { unlink; } from 'fs-extra';
 
 export const handlerValidation = (schema: SchemaMap, check: string) => {
    return (req: Request, res: Response, next: NextFunction) => {
       if (check === 'body') {
          const error = validate(req.body, schema);
-         next(error);
+         next(error?.details);
       }
       if (check === 'params') {
          const error = validate(req.params, schema);
-         next(error);
+         next(error?.details);
       }
    };
 };
