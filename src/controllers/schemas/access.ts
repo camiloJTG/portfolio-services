f117f1a67befde45f7696555972ee449a3564831
camiloJTG <camilojtg.dev@gmail.com>
2021-01-01 13:27:56 -0300

    feat: Create schema for validation inputs data

---
+++ src/controllers/schemas/access.ts
@@ -0,0 +1,13 @@
+import joi from 'joi';
+
+// Login data
+const mailSchema = joi.string().max(500).min(1).trim();
+const passwordSchema = joi.string().max(500).min(1).trim();
+
+// Id mongo
+export const mongoIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+
+export const loginSchema = {
+    mail: mailSchema.required(),
+    password: passwordSchema.required()
+};
---
+++ src/controllers/schemas/personal.ts
@@ -0,0 +1,45 @@
+import joi from 'joi';
+
+// Account data
+const mailSchema = joi.string().max(500).min(1).trim();
+const passwordSchema = joi.string().max(500).min(1).trim();
+const usernameSchema = joi.string().max(500).min(1).trim();
+
+// Social media data
+const fullNameSchema = joi.string().max(100).min(1).trim();
+const prioritySchema = joi.number();
+const urlSchema = joi.string().max(100000000).min(1).trim();
+const socialMedia = joi.array();
+
+// Id mongo
+export const mongoIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+
+export const createPersonalSchema = {
+   account: {
+      mail: mailSchema.required(),
+      password: passwordSchema.required(),
+      username: usernameSchema.required(),
+   },
+   socialMedia: socialMedia.items(
+      joi.object({
+         fullName: fullNameSchema.required(),
+         priority: prioritySchema.required(),
+         url: urlSchema.required(),
+      })
+   ),
+};
+
+export const updatePersonalSchema = {
+   account: {
+      mail: mailSchema,
+      password: passwordSchema,
+      username: usernameSchema,
+   },
+   socialMedia: socialMedia.items(
+      joi.object({
+         fullName: fullNameSchema,
+         priority: prioritySchema,
+         url: urlSchema,
+      })
+   ),
+};
