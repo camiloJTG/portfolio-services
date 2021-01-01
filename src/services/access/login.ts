5bcc3865224da79e96bc4b5aa1fecb2fa8124404
camiloJTG <camilojtg.dev@gmail.com>
2021-01-01 13:08:33 -0300

    feat: Create login access and token

---
+++ src/services/access/login.ts
@@ -0,0 +1,30 @@
+import { ILogin } from '../../interfaces/access';
+import accountModel from '../../models/personal/accounts';
+import { validateHash } from '../../utils/bcrypt';
+import { createToken } from '../../utils/jwt';
+
+export const login = async (login: ILogin) => {
+   try {
+      // Validate if mail is correct
+      const findMail = await accountModel
+         .findOne({ mail: login.mail })
+         .lean();
+      if (!findMail) return `Invalid credentials`;
+
+      // Validate if password is correct
+      const comparePassword = await validateHash(
+         login.password,
+         findMail.password
+      );
+      if (!comparePassword) return 'Invalid credentials';
+
+      // Generate token with data personal
+      const { mail, username, _id } = findMail;
+      const newToken = { mail, username, _id };
+      const token = createToken(newToken);
+
+      return token;
+   } catch (e) {
+      console.error(e.message);
+   }
+};
