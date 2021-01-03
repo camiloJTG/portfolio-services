95d201dc521d98cd7d35612c924fd2fedecee763
camiloJTG <camilojtg.dev@gmail.com>
2021-01-03 17:40:31 -0300

    feat: Implement routes for media services

--- src/controllers/routes/index.ts
+++ src/controllers/routes/index.ts
@@ -1,19 +1,21 @@
 import swaggerUI from 'swagger-ui-express';
 import swagger from '../../utils/swagger';
-
-import users from './users/users';
-import languages from './users/languages';
-import socialMedia from './users/socialMedia';
-import account from './users/accounts';
-
+import personal from './personal';
+import access from './access';
+import project from './projects/projects';
+import application from './projects/applications';
+import tools from './projects/tools';
+import media from './media';
 import { Application } from 'express';
 
 const routers = (app: Application) => {
    const prefix = '/api';
-   app.use(`${prefix}/users`, users);
-   app.use(`${prefix}/languages`, languages);
-   app.use(`${prefix}/socialsMedia`, socialMedia);
-   app.use(`${prefix}/accounts`, account);
+   app.use(`${prefix}/access`, access);
+   app.use(`${prefix}/medias`, media);
+   app.use(`${prefix}/personal`, personal);
+   app.use(`${prefix}/projects`, project);
+   app.use(`${prefix}/applications`, application);
+   app.use(`${prefix}/tools`, tools);
    app.use(`${prefix}/docs/`, swaggerUI.serve, swaggerUI.setup(swagger));
 };
 
