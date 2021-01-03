724f4f6d7a42187d807c5d779a4800bd155af757
camiloJTG <camilojtg.dev@gmail.com>
2021-01-03 17:42:13 -0300

    feat: Create middleware for multer module

---
+++ src/controllers/middlewares/handlerFile.ts
@@ -0,0 +1,12 @@
+import multer from 'multer';
+import { extname } from 'path';
+import { v4 } from 'uuid';
+
+const storage = multer.diskStorage({
+    destination: 'src/controllers/public/images',
+    filename: (req, file, cb) => {
+        cb(null, v4() + extname(file.originalname));
+    }
+});
+
+export default multer({ storage });
